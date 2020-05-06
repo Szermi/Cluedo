648d18a2e4307881f3346a79924f753fc6641c8b
Wojciech Żółtowski <zolwoj@wp.pl>
2020-05-06 14:51:33 +0200

    Added Murder Tools pickup

---
+++ Assets/Scripts/MurderItemsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MurderItemsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the object
+        this.transform.Rotate(0.0f, 10.0f, 0.0f, Space.Self);
+    }
+}

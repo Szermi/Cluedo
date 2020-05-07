53757185ebc89c0e4bedb6dee767fe7344373ca7
Bras <adamzieli@wp.pl>
2020-05-07 09:33:41 +0200

    Add assets

---
+++ Assets/Scripts/BottleRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the object
+        this.transform.Rotate(2.0f, 0.0f, 2.0f, Space.Self);
+    }
+}

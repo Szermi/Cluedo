65c79b5c0d736dc5e8d27348b709043d2a141cfb
Wojciech Żółtowski <zolwoj@wp.pl>
2020-06-03 16:08:22 +0200

    Added door on button(torch)

---
+++ Assets/Scripts/DoorMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMove : MonoBehaviour
+{
+    public Vector3 targetPosition = new Vector3(1.2f, -5.625f, -17.95f);
+    public float speed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject Torch = GameObject.Find("OpenDoorTorch");
+            Torch.transform.Rotate(20.0f, 00.0f, 0.0f, Space.Self);
+            Vector3 move = new Vector3(0, -0.15f, 0);
+            Torch.transform.position += move;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject Door = GameObject.Find("HouseDoorClick");
+            Door.transform.localPosition = Vector3.Lerp(Door.transform.localPosition, targetPosition, Time.deltaTime * speed);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject Torch = GameObject.Find("OpenDoorTorch");
+            Torch.transform.Rotate(-20.0f, 00.0f, 0.0f, Space.Self);
+            Vector3 move = new Vector3(0, 0.15f, 0);
+            Torch.transform.position += move;
+        }
+    }
+}
